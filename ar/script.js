2f68ff2fb56b0ac9cffaaa4e7d7bfe76c5251eaf
Sarah Carneiro <sarah.rodriguesc93@gmail.com>
2026-01-24 16:32:36 -0300

    Update script.js

--- ar/script.js
+++ ar/script.js
@@ -42,4 +42,46 @@ console.log(Boolean(nome));
 console.log(Boolean(idade));
 console.log(Boolean(possuiPremio));
 console.log(Boolean(empregoFuturo));
+
 console.log(Boolean(dinheiroNaConta));
+
+let diaDaSemana;
+let nomeDia;
+
+diaDaSemana = prompt("Qual é o dia da semana em numero:", "numero");
+
+switch(diaDaSemana){
+    case '1':
+        nomeDia = "Domingo";
+        break
+    
+    case '2':
+        nomeDia = "Segunda";
+        break;
+
+    case '3':
+        nomeDia = "Terça";
+        break;
+
+    case '4':
+        nomeDia = "Quarta";
+        break;
+
+    case '5':
+        nomeDia = "Quinta";
+        break;
+
+    case '6':
+        nomeDia = "Sexta";
+        break;
+
+    case '7':
+        nomeDia = "Sabado";
+        break;
+
+    default:
+        nomeDia = "Dia invalido";
+        break;
+}
+
+console.log(nomeDia);
